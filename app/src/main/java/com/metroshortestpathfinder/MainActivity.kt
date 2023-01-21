1f058ff7078998b9796a1399a83bd9cf20af42dc
Amin <amin.sakha00@gmail.com>
2023-01-21 23:32:24 +0330

    add string lines

--- app/src/main/java/com/metroshortestpathfinder/MainActivity.kt
+++ app/src/main/java/com/metroshortestpathfinder/MainActivity.kt
@@ -7,5 +7,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val a = resources.getStringArray(R.array.firstLine)
+        println(a)
+        println(a[1])
     }
 }
