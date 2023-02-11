b55fd61e46d233604a2c0ab11826a1a99ebfef41
Amin <amin.sakha00@gmail.com>
2023-02-11 22:10:36 +0330

    getting ready for creating UI

--- app/src/main/java/com/metroshortestpathfinder/Db.kt
+++ app/src/main/java/com/metroshortestpathfinder/Db.kt
@@ -1,8 +1,5 @@
 package com.metroshortestpathfinder
 
-import java.util.ArrayList
-
 val stationList = mutableListOf<Station>()
-var lastResult: Result? = null
 val idListResult= mutableListOf<Int>()
 val graph = Graph()
